890ab57b8e39a501611b2e63eb55e5383bbf429c
thanatos <523928638@qq.com>
2016-09-04 21:20:37 +0800

    测试提交

---
+++ utilslibrary/src/main/java/thanatos/utilslibrary/view/CustomListView.java
@@ -0,0 +1,23 @@
+package thanatos.utilslibrary.view;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.ListView;
+
+/**
+ * Created by lxf52 on 2016/9/4.
+ */
+
+public class CustomListView extends ListView {
+    public CustomListView(Context context) {
+        super(context);
+    }
+
+    public CustomListView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public CustomListView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
